201916ee71e2b14b52e7c8510e7a4023f6576aae
reda <reda@localhost>
2017-06-11 23:13:14 +0300

    twitch streaming

--- stream_screen.sh
+++ stream_screen.sh
@@ -11,26 +11,23 @@ CAMERA_HEIGHT=90
 _cam_xpos=$(($FULLSCREEN_WIDTH-$CAMERA_WIDTH-1))
 _cam_ypos=0
 
-
 # See 'pacmd list-sources' to find proper values
 PULSE_AUDIO_MIC="cras-source"
 PULSE_AUDIO_MON="cras-sink.monitor"
 
+TWITCH_KEY=$(cat ~/.twitchkey)
+STREAM_DESTINATION="rtmpsink location=rtmp://live-ams.twitch.tv/app/${TWITCH_KEY}"
 
-ENCODER1=(
-  x264enc
-  bitrate=768
-  speed-preset=faster # CPU/quality tradeoff
-  qp-min=30 # quality/VBV-underflow tradeoff
-  tune=zerolatency
-)
+echo $STREAM_DESTINATION
 
-MUXER="flvmux streamable=true name=mux"
+FILE_DESTINATION="filesink location=data.flv"
 
-gst-launch-1.0 -v \
+ENCODER="x264enc bitrate=700 speed-preset=faster qp-min=30 tune=zerolatency"
+
+gst-launch-1.0 \
 videomixer name=vmix sink_1::xpos=${_cam_xpos} sink_1::ypos=${_cam_ypos} !\
-queue leaky=downstream ! ${ENCODER1[@]} ! \
-${MUXER} ! filesink location=debug.flv \
+queue leaky=downstream ! ${ENCODER} ! \
+flvmux streamable=true name=mux ! ${STREAM_DESTINATION} \
 audiomixer name=amix ! queue leaky=downstream ! voaacenc ! mux. \
 ximagesrc use-damage=0 endx=${_endx} endy=${_endy} ! \
 video/x-raw, framerate=30/1 ! \
@@ -50,4 +47,3 @@ audio/x-raw,channels=1 ! amix. \
 pulsesrc device=${PULSE_AUDIO_MIC} ! \
 audioconvert ! \
 audio/x-raw,channels=1 ! amix.
-
