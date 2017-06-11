98774a156c3aad42b2494350e9b6f9db2e79b2a8
reda <reda@localhost>
2017-06-11 22:16:25 +0300

    web-camera video support. mic support

--- stream_screen.sh
+++ stream_screen.sh
@@ -5,6 +5,13 @@ FULLSCREEN_HEIGHT=480
 _endx=$(($FULLSCREEN_WIDTH-1))
 _endy=$(($FULLSCREEN_HEIGHT-1))
 
+CAMERA_WIDTH=160
+CAMERA_HEIGHT=90
+
+_cam_xpos=$(($FULLSCREEN_WIDTH-$CAMERA_WIDTH-1))
+_cam_ypos=0
+
+
 # See 'pacmd list-sources' to find proper values
 PULSE_AUDIO_MIC="cras-source"
 PULSE_AUDIO_MON="cras-sink.monitor"
@@ -21,15 +28,26 @@ ENCODER1=(
 MUXER="flvmux streamable=true name=mux"
 
 gst-launch-1.0 -v \
+videomixer name=vmix sink_1::xpos=${_cam_xpos} sink_1::ypos=${_cam_ypos} !\
+queue leaky=downstream ! ${ENCODER1[@]} ! \
 ${MUXER} ! filesink location=debug.flv \
-ximagesrc use-damage=0 endx=${_endx} endy=${_endy} !\
-video/x-raw, framerate=30/1 !\
+audiomixer name=amix ! queue leaky=downstream ! voaacenc ! mux. \
+ximagesrc use-damage=0 endx=${_endx} endy=${_endy} ! \
+video/x-raw, framerate=30/1 ! \
 videoscale method=0 add-borders=false ! \
 video/x-raw,width=${FULLSCREEN_WIDTH},height=${FULLSCREEN_HEIGHT} !\
 videoconvert ! \
-queue leaky=downstream ! ${ENCODER1[@]} ! mux. \
-pulsesrc device=${PULSE_AUDIO_MON} !\
+vmix. \
+v4l2src ! \
+video/x-raw, framerate=30/1 ! \
+videoscale method=0 add-borders=false ! \
+video/x-raw,width=${CAMERA_WIDTH},height=${CAMERA_HEIGHT} !\
+videoconvert ! \
+vmix. \
+pulsesrc device=${PULSE_AUDIO_MON} ! \
 audioconvert ! \
-audio/x-raw,channels=1 ! \
-queue leaky=downstream ! voaacenc ! mux.
+audio/x-raw,channels=1 ! amix. \
+pulsesrc device=${PULSE_AUDIO_MIC} ! \
+audioconvert ! \
+audio/x-raw,channels=1 ! amix.
 
