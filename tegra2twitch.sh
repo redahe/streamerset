e8687d17201447aca271ff1d823fc6405e34e113
redahe <redaworld@gmail.com>
2017-06-14 21:12:42 +0000

    renamed params moved

--- tegra2twitch.sh
+++ tegra2twitch.sh
@@ -4,8 +4,8 @@
 # Streams from a TV-connected board to Twitch                #
 ##############################################################
 
-DEST_WIDTH=480
-DEST_HEIGHT=270
+DEST_WIDTH=720
+DEST_HEIGHT=405
 
 TWITCH_KEY=$(cat ~/.twitchkey)
 STREAM_DESTINATION="rtmpsink location=rtmp://live-ams.twitch.tv/app/${TWITCH_KEY}"
@@ -16,7 +16,7 @@ FILE_DESTINATION="filesink location=data.flv"
 
 # See 'pacmd list-sources' to find proper values
 PULSE_AUDIO_MON="alsa_output.platform-tegra30-hda.hdmi-stereo.monitor"
-ENCODER="omxh264enc control-rate=2 bitrate=600000" 
+ENCODER="omxh264enc control-rate=2 bitrate=700000" 
 QUEUE="queue leaky=downstream "
 
 DISPLAY=:0 gst-launch-1.0 \
@@ -27,6 +27,6 @@ audio/x-raw,channels=1 ! ${QUEUE} ! voaacenc ! \
 flvmux streamable=true name=mux ! \
 ${QUEUE} ! \
 ${FILE_DESTINATION} \
-ximagesrc use-damage=0 endx=960 endy=540 ! \
-nvvidconv ! 'video/x-raw(memory:NVMM), width=480, height=270, framerate=60/1' ! \
+ximagesrc use-damage=0 endx=1920 endy=1080 ! \
+nvvidconv ! 'video/x-raw(memory:NVMM), width='${DEST_WIDTH}', height='${DEST_HEIGHT}', framerate=60/1' ! \
 ${ENCODER} ! mux.
